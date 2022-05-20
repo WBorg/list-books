091064fed5b8f74d90a9d145aa52324e87ea6c52
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-05-20 19:02:38 -0300

    www

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import formCss from './form.module.css';
+import {Button} from 'react-bootstrap';
 
 function FormAdd() {
 
@@ -7,29 +8,30 @@ function FormAdd() {
 
 
     return ( 
-        <>
+        <section className={formCss.wrapper}>
             <h1 className={formCss.title}>Adicionar Livro</h1>
            <div>
                <form className={formCss.form} action="">
                     <div>
-                        <label htmlFor="title"> Título: </label>
+                        <label htmlFor="title"> Título </label>
                         <input type="text" id="title" />
                     </div>
                     <div>
-                        <label htmlFor="img"> Imagem: </label>
+                        <label htmlFor="img"> Imagem </label>
                         <input type="text" id="img" />
                     </div>
                     <div>
-                        <label htmlFor="price"> Preço: </label>
+                        <label htmlFor="price"> Preço </label>
                         <input type="text" id="price" />
                     </div>
                     <div>
-                        <label htmlFor="url"> URL: </label>
+                        <label htmlFor="url"> URL </label>
                         <input type="text" id="url" />
                     </div>
+                    <Button className={formCss.btn} variant="danger">Adicionar</Button>
                </form>
            </div>
-        </>
+        </section>
      );
 }
 
